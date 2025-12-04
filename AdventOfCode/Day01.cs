c9cd6caa0bed9b27d22210a64530ed70b366095f
Liam <108070429+GangleAbout@users.noreply.github.com>
2025-12-04 20:58:23 +0000

    Add Day 1 Part 2 Solution

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -8,7 +8,7 @@ public class Day01 : BaseDay
 
         var dialValue = 50; // Initial condition
         var zeroCount = 0;
-        
+
         foreach (var input in inputs)
         {
             var spin = int.Parse(input[1..]);
@@ -18,78 +18,42 @@ public class Day01 : BaseDay
             if (dialValue % 100 == 0)
             {
                 zeroCount++;
-            } 
+            }
         }
-        
+
         return new ValueTask<string>(zeroCount.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        var inputs = File.ReadAllText("Inputs/01.txt").Split("\n");
+
+        var dialValue = 50;
+        var zeroCount = 0;
+
+        foreach (var input in inputs)
+        {
+            var spin = int.Parse(input[1..]);
+
+            // Hacky...
+            for (var i = 1; i <= spin; i++)
+            {
+                if (input[0] == 'R')
+                {
+                    dialValue = (dialValue - 1) % 100;
+                }
+                else
+                {
+                    dialValue = (dialValue + 1) % 100;
+                }
+
+                if (dialValue == 0)
+                {
+                    zeroCount++;
+                }
+            }
+        }
+
+        return new ValueTask<string>(zeroCount.ToString());
     }
 }
-
-// public class Day01 : BaseDay
-// {
-//     private static int Overflow(int val, int range)
-//     {
-//         return val % range;
-//     }
-//
-//     private static int Underflow(int val, int range)
-//     {
-//         return (val % range + range) % range;
-//     }
-//
-//     public override ValueTask<string> Solve_1()
-//     {
-//         var zeroCount = 0;
-//         var dialVal = 50;
-//
-//         const int min = 0;
-//         const int max = 99;
-//
-//         var input = File.ReadAllText("Inputs/01.txt").Split("\n");
-//
-//         foreach (var i in input)
-//         {
-//             _ = int.TryParse(i.AsSpan(1), out var spin);
-//
-//             dialVal += i[0] == 'R' ? spin : -spin;
-//
-//             dialVal = dialVal switch
-//             {
-//                 > max => Overflow(dialVal, max + 1),
-//                 < min => Underflow(dialVal, max + 1),
-//                 _ => dialVal
-//             };
-//
-//             if (dialVal == 0) zeroCount++;
-//         }
-//
-//         return new ValueTask<string>(zeroCount.ToString());
-//     }
-//
-//     public override ValueTask<string> Solve_2()
-//     {
-//         var zeroCount = 0;
-//         var dialVal = 50;
-//
-//         const int min = 0;
-//         const int max = 99;
-//
-//         var input = File.ReadAllText("Inputs/01.txt").Split("\n");
-//
-//         foreach (var i in input)
-//         {
-//             _ = int.TryParse(i.AsSpan(1), out var spin);
-//
-//             dialVal += i[0] == 'R' ? spin : -spin;
-//
-//             
-//         }
-//
-//         return new ValueTask<string>(zeroCount.ToString());
-//     }
-// }
