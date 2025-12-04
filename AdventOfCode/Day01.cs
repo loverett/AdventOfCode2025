323a4f0f1d2a32d773e75b5b6da35499f86516ae
Liam <108070429+GangleAbout@users.noreply.github.com>
2025-12-04 10:19:47 +0000

    Add Day 1 Part 1 Solution

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,49 @@
-﻿namespace AdventOfCode;
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
-
-    public Day01()
+    private static int Overflow(int val, int range)
     {
-        _input = File.ReadAllText(InputFilePath);
+        return val % range;
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    private static int Underflow(int val, int range)
+    {
+        return (val % range + range) % range;
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_1()
+    {
+        var zeroCount = 0;
+        var dialVal = 50;
+
+        const int min = 0;
+        const int max = 99;
+
+        var input = File.ReadAllText("Inputs/01.txt").Split("\n");
+
+
+        foreach (var i in input)
+        {
+            _ = int.TryParse(i.AsSpan(1), out var spin);
+
+            dialVal += i[0] == 'R' ? spin : -spin;
+
+            dialVal = dialVal switch
+            {
+                > max => Overflow(dialVal, max + 1),
+                < min => Underflow(dialVal, max + 1),
+                _ => dialVal
+            };
+
+            if (dialVal == 0) zeroCount++;
+        }
+
+        return new ValueTask<string>(zeroCount.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
 }
