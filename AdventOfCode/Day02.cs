8eebd7a562e8f5dc1c49d8154fbca44dad7a1c1b
Liam <108070429+GangleAbout@users.noreply.github.com>
2025-12-05 12:04:14 +0000

    Add Day 2 Part 2 Solution

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -1,44 +1,35 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode;
 
 public class Day02 : BaseDay
 {
     private string[] _inputs = File.ReadAllText("Inputs/02.txt").Split(",");
 
-    private static long CountDigit(long n)
-    {
-        return (long)Math.Floor(Math.Log10(n) + 1);
-    }
-
     public override ValueTask<string> Solve_1()
     {
-        long totalInvalid = 0;
+        ulong totalInvalid = 0;
 
         foreach (var range in _inputs)
         {
             var ranges = range.Split("-");
-            var min = long.Parse(ranges[0]);
-            var max = long.Parse(ranges[1]);
+            var min = ulong.Parse(ranges[0]);
+            var max = ulong.Parse(ranges[1]);
 
             for (var i = min; i <= max; i++)
             {
-                // No leading zeros but "just in case"
-                if (i == 0) continue;
-
-                var numDigits = CountDigit(i);
+                var iAsStr = i.ToString();
+                var numDigits = iAsStr.Length;
 
                 if (numDigits % 2 != 0) continue;
 
-                // Convert to string
-                var iAsStr = i.ToString();
-
-                var firstHalf = numDigits > 2 ? iAsStr[..(int)(numDigits / 2)] : iAsStr[..1];
-                var secondHalf = iAsStr[(int)(numDigits / 2)..];
+                var firstHalf = iAsStr[..(numDigits / 2)];
+                var secondHalf = iAsStr[(numDigits / 2)..];
 
                 // Compare two ranges
-                if (string.Equals(firstHalf, secondHalf))
-                {
-                    totalInvalid += i;
-                }
+                if (!string.Equals(firstHalf, secondHalf)) continue;
+                
+                totalInvalid += i;
             }
         }
 
@@ -47,6 +38,36 @@ public class Day02 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        ulong totalInvalid = 0;
+
+        foreach (var range in _inputs)
+        {
+            var ranges = range.Split("-");
+            var min = ulong.Parse(ranges[0]);
+            var max = ulong.Parse(ranges[1]);
+
+            for (var i = min; i <= max; i++)
+            {
+                var iAsStr = i.ToString();
+                var numDigits = iAsStr.Length;
+
+                var subDigits = iAsStr[..(numDigits / 2)];
+
+                for (var j = (numDigits / 2) - 1; j >= 0; j--)
+                {
+                    var matchesCount = Regex.Matches(iAsStr, subDigits);
+
+                    if (matchesCount.Count * subDigits.Length == iAsStr.Length)
+                    {
+                        totalInvalid += i;
+                        break;
+                    }
+
+                    subDigits = subDigits[..^1];
+                }
+            }
+        }
+
+        return new ValueTask<string>(totalInvalid.ToString());
     }
 }
