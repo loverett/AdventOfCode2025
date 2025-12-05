43fe600309aa7a18ca6e3cb7dc3f1561364927e1
Liam <108070429+GangleAbout@users.noreply.github.com>
2025-12-05 13:12:15 +0000

    Add Day 3 Part 2 Solution

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -32,6 +32,31 @@ public class Day03 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        ulong totalMaxJoltage = 0;
+
+        foreach (var input in _inputs)
+        {
+
+            var batteriesFound = 0;
+            char[] joltage = { '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'};
+            var lastIndex = -1;
+
+            while (batteriesFound < 12)
+            {
+                for (var i = lastIndex + 1; i < input.Length - 11 + batteriesFound; i++)
+                {
+                    if (input[i] <= joltage[batteriesFound]) continue;
+                    
+                    joltage[batteriesFound] = input[i];
+                    lastIndex = i;
+                }
+
+                batteriesFound++;
+            }
+            
+            totalMaxJoltage += ulong.Parse(joltage);
+        }
+
+        return new ValueTask<string>(totalMaxJoltage.ToString());
     }
 }
