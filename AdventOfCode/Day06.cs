8a373df370a3d6d5fcf51455840fa872d714a5d9
Liam Overett <108070429+loverett@users.noreply.github.com>
2025-12-14 19:27:10 +0000

    Add Day 6 Part 1 Solution

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode.Utilities.Extensions;
+
+namespace AdventOfCode;
+
+public class Day06 : BaseDay
+{
+    private string[][] GetInput()
+    {
+        var input = File.ReadAllLines("Inputs/06.txt");
+
+        var inputJagged = new string[input.Length][];
+        foreach (var line in input.WithIndex())
+        {
+            inputJagged[line.Index] = line.Item.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        return inputJagged;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var inputRaw = GetInput();
+        var input = inputRaw[0]
+            .Select((_, colIdx) => inputRaw.Select(row => row[colIdx]).ToArray())
+            .ToArray();
+
+        ulong totalAnswer = 0;
+
+        foreach (var row in input)
+        {
+            var operand = row.Last();
+
+            var rowTotal = operand == "*" ? 1UL : 0UL;
+
+            foreach (var num in row.SkipLast(1))
+            {
+                var numAsUlong = ulong.Parse(num);
+
+                if (operand == "*")
+                {
+                    rowTotal *= numAsUlong;
+                }
+                else
+                {
+                    rowTotal += numAsUlong;
+                }
+            }
+
+            totalAnswer += rowTotal;
+        }
+
+        return new ValueTask<string>(totalAnswer.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdventOfCode/Utilities/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Utilities.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<(T Item, int Index)> WithIndex<T>(this IEnumerable<T> source)
+    {
+        return source.Select((item, index) => (item, index));
+    }
+}
