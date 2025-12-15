356e75cea23feda8ced768f777c0b65f07f5fe2b
Liam Overett <108070429+loverett@users.noreply.github.com>
2025-12-15 20:20:48 +0000

    Add Day 7 Part 2 Solution

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -48,6 +48,39 @@ public class Day07 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        var inputRaw = File.ReadAllLines("Inputs/07.txt");
+        var input = new char[inputRaw.Length][];
+
+        foreach (var line in inputRaw.WithIndex())
+        {
+            input[line.Index] = line.Item.ToCharArray();
+        }
+        
+        var paths = new ulong[input[0].Length];
+        
+        foreach (var c in input[0].WithIndex())
+        {
+            if (c.Item is 'S' or 's')
+            {
+                paths[c.Index] = 1;
+            }
+        }
+
+        for (var row = 2; row < input.Length; row += 2)
+        {
+            foreach (var (item, index) in input[row].WithIndex())
+            {
+                if (item != '^') continue;
+                
+                paths[index - 1] = paths[index] + paths[index - 1];
+                paths[index + 1] = paths[index] + paths[index + 1];
+
+                paths[index] = 0;
+            }
+        }
+
+        var sum = paths.Aggregate(0UL, (current, val) => current + val);
+
+        return new ValueTask<string>(sum.ToString());
     }
 }
